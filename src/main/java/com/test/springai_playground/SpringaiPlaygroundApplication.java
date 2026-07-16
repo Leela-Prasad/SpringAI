62f554259ca102ecab42869cb28aae7ff45751ed
Leela-Prasad <leela.jagu@gmail.com>
2026-07-16 17:13:26 +0530

    Initial Project Setup

---
+++ src/main/java/com/test/springai_playground/SpringaiPlaygroundApplication.java
@@ -0,0 +1,13 @@
+package com.test.springai_playground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringaiPlaygroundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringaiPlaygroundApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/test/springai_playground/SpringaiPlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.springai_playground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringaiPlaygroundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
