4c64e1633af42c9bdb7d94a7e7074d2d1ceaa9db
Leela-Prasad <leela.jagu@gmail.com>
2026-07-16 20:55:01 +0530

    ChatOptions

---
+++ src/test/java/com/test/springai_playground/sec01/Lec04ChatOptionsTest.java
@@ -0,0 +1,34 @@
+package com.test.springai_playground.sec01;
+
+import com.test.springai_playground.AbstractTest;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.ai.chat.prompt.ChatOptions;
+
+public class Lec04ChatOptionsTest extends AbstractTest {
+
+    private static final Logger log = LoggerFactory.getLogger(Lec04ChatOptionsTest.class);
+
+    @ParameterizedTest
+    @ValueSource(ints = {5, 10})
+    public void chatOptionsTest(int tokens) {
+        String prompt = "3 + 4 = ?";
+        var builder = ChatOptions
+                .builder()
+                .maxTokens(tokens);
+
+        runPrompt(prompt, builder);
+    }
+
+
+    private void runPrompt(String prompt, ChatOptions.Builder builder) {
+        var response = client.prompt(prompt)
+                .options(builder)
+                .call()
+                .chatResponse();
+
+        log.info("Response: {}", response);
+    }
+}
